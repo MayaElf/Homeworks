4206858b4d228137d71f6b18c6ef6a8ea6aad35b
Maya Afanaseva <afanasevaml@yandex.ru>
2023-10-02 14:31:03 +0300

    6th Task_Python_Generator

---
+++ Homeworks/Homework2/src/main/hw6_2.py
@@ -0,0 +1,39 @@
+# 2. Implement a map-like function that returns an iterator (generator function)
+# extra functionality: if arg function can't be applied, return element as is + text exception
+#
+# def map_like(fun, *elems):
+#     pass
+#
+# # example input
+# a = [1, 2, 3]
+# b = 6
+# c = 'zhaba'
+# d = True
+# fun = lambda x: x[0]
+#
+# for _ in map_like(fun, a, b, c, d):
+#     print(_)
+
+# output:
+# 1
+# 6: 'int' object is not subscriptable
+# z
+# True: 'bool' object is not subscriptable
+
+def map_like(fun, *elems):
+    for elem in elems:
+        try:
+            yield fun(elem)
+        except Exception as e:
+            yield f"{elem}: {str(e)}"
+
+# example input
+if __name__ == "__main__":
+    a = [1, 2, 3]
+    b = 6
+    c = 'zhaba'
+    d = True
+    fun = lambda x: x[0]
+
+for _ in map_like(fun, a, b, c, d):
+    print(_)
